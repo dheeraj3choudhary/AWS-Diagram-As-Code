35db2b0de89aedce33b133e47b2575c3ce2d7b56
Dheeraj Choudhary <88716270+dheeraj3choudhary@users.noreply.github.com>
2022-05-20 22:55:11 +0530

    Create no_direction_flow.py

---
+++ no_direction_flow.py
@@ -0,0 +1,8 @@
+# Author :- Dheeraj Choudhary
+from diagrams import Diagram
+from diagrams.aws.compute import EC2
+from diagrams.aws.database import RDS
+from diagrams.aws.network import ELB
+
+with Diagram("Diagram", show= True, direction="LR"):
+    ELB("Load_Balancer") - EC2("Instance") - RDS("Database")
